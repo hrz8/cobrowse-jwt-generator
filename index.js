e88357164b1c5a0d0382770bb30d92a25985945e
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-05-17 19:51:22 +0700

    add argumanet acceptor ～

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
+const args = process.argv.slice(2);
+if (args.length < 1) process.exit(0);
+
 const privateKey = fs.readFileSync('private.pem');
 const token = jwt.sign({
     iat: Date.now(),
@@ -10,4 +13,5 @@ const token = jwt.sign({
     sub: 'your@email.com',
     displayName: 'Your Name',
 }, {key: privateKey, passphrase: 'pwd'}, { algorithm: 'RS256' });
+
 console.log(token);
